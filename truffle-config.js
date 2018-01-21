296a14a197438b8ba48aac74e5bea520e413d7dc
gdpr-cash-dev <dev@gdpr.cash>
2018-01-21 18:17:06 +0200

    basic structure created

--- truffle-config.js
+++ truffle-config.js
@@ -2,8 +2,9 @@ module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration> 
     networks: {
         development: {
-            host: "localhost",
-            port: 8545,
+            host: "127.0.0.1",
+            port: 7545,
+            gas: 4600000,
             network_id: "*" // Match any network id
         }
     }
