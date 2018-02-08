a8537fe2fde6d8096bae2494b7c3aefabefbbd83
gdpr-cash-dev <dev@gdpr.cash>
2018-02-08 18:08:37 +0200

    some tests removed

--- test/GDPRCash.js
+++ test/GDPRCash.js
@@ -62,22 +62,18 @@ contract('GDPRCash (Basic Tests)', function (accounts) {
         throw new Error("a regular user transferred before they were enabled")
     });
 
-    it("should allow the admin and the crowdsale to make transfers", async function () {
+    it("should allow the admin to make transfers", async function () {
         await token.transferFrom(owner, user2, 1, {from: admin});
-        await token.transferFrom(owner, user3, 1, {from: sale.address});
         let ownerBalance = await token.balanceOf(owner);
         let initialSupply = await token.INITIAL_SUPPLY();
         let totalSupply = await token.totalSupply();
         let user2Balance = await token.balanceOf(user2);
-        let user3Balance = await token.balanceOf(user3);
         ownerBalance = ownerBalance.toNumber();
         initialSupply = initialSupply.toNumber();
         totalSupply = totalSupply.toNumber();
         user2Balance = user2Balance.toNumber();
-        user3Balance = user3Balance.toNumber();
         assert.equal(user2Balance, 1, "user2 should have 1 GDPRs");
-        //assert.equal(user3Balance, 1, "user3 should have 1 GDPRs");
-        //assert.equal(ownerBalance, totalSupply-2, "the owner should now have 30% of the original funds");
+        assert.equal(ownerBalance, totalSupply-1, "the total amount should be subtracted by 1");
         assert.equal(totalSupply, initialSupply, "the total supply should equal the initial supply");
     });
 
@@ -116,8 +112,8 @@ contract('GDPRCash (token burning tests)', function (accounts) {
         let transferEnabled = await token.transferEnabled();
         assert(!transferEnabled);
 
-        // Owner transfers 10 tokens to user1
-        await token.transfer(user2, 10, {from: admin});
+        // Owner transfers 10 tokens to user2
+        await token.transferFrom(owner, user2, 10, {from: admin});
         let balance = await token.balanceOf(user2);
         assert.equal(balance, 10);
 
@@ -132,11 +128,11 @@ contract('GDPRCash (token burning tests)', function (accounts) {
     });
 });
 
-/*
+
 contract('GDPRCash (Transfer Ownership Tests)', function(accounts) {
   // account[0] points to the owner on the testRPC setup
   var owner = accounts[0];
-  var user1 = accounts[1];
+  var admin = accounts[1];
   var user2 = accounts[2];
   var user3 = accounts[3];
 
@@ -147,33 +143,6 @@ contract('GDPRCash (Transfer Ownership Tests)', function(accounts) {
       initialSupply = initialSupply.toNumber();
       ownerBalance = ownerBalance.toNumber();
       assert.equal(ownerBalance, initialSupply, "the owner's balance should be the totalSupply");
-  });
-
-
-
-  it("should be able to transfer all tokens from owner to crowdsale and back", async function() {
-      let token = await GDPRCash.deployed();
-      let sale = await GDPRCashSale.deployed();
-      let saleAddr = await sale.address;
-
-      // owner transfers tokens to crowdsale address
-      token.transferTokens(saleAddr);
-
-      let initialSupply = (await token.INITIAL_SUPPLY()).toNumber();
-      let saleBalance = (await token.balanceOf(saleAddr)).toNumber();
-      let ownerBalance = (await token.balanceOf(owner)).toNumber();
-
-      assert.equal(saleBalance, initialSupply, "the crowdsale should now have all tokens");
-      assert.equal(ownerBalance, 0, "the original owner should now have zero tokens");
-
-      // owner transfers tokens back to itself
-      token.transferTokens(owner);
-
-      saleBalance = (await token.balanceOf(saleAddr)).toNumber();
-      ownerBalance = (await token.balanceOf(owner)).toNumber();
-
-      assert.equal(ownerBalance, initialSupply, "the owner should have all the tokens");
-      assert.equal(saleBalance, 0, "the crowdsale shouldn't have any tokens");
   });
 
   it("should not allow a regular user to transfer ownership", async function() {
@@ -198,7 +167,3 @@ contract('GDPRCash (Transfer Ownership Tests)', function(accounts) {
       assert.equal(saleAddr, tokenOwnerAfter, "the token should be owned by the crowdsale after transferOwnership");
   });
 });
-
-
-});
-*/
