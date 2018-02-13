aad0c3e628690d3002e45869e2c5b81fe0992d50
gdpr-cash-dev <dev@gdpr.cash>
2018-02-13 15:34:01 +0200

    solc optimizer added

--- truffle-config.js
+++ truffle-config.js
@@ -10,6 +10,12 @@ if (fs.existsSync('accounts.json')) {
 
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration> 
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200
+        }
+    },
     networks: {
         development: {
             host: "127.0.0.1",
